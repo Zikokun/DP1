9e4b94c01a0b46c48a8c599e7159884f3cdf41f4
Alumno de Laboratorio de Informática <alulab@V20806.lab.inf.pucp.edu.pe>
2016-10-22 14:57:57 -0500

    primer comit

---
+++ src/test1_gui_alg/Test1_gui_alg.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1_gui_alg;
+import vista.NuevaVentana;
+/**
+ *
+ * @author alulab
+ */
+public class Test1_gui_alg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NuevaVentana nv= new NuevaVentana();
+        nv.setVisible(true);
+    }
+    
+}
