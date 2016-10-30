e5319104221c48f0bba42204ea85242463620596
viivigood <vivian0306@hotmail.com>
2016-10-30 14:54:08 -0500

    leer Aeropuertos

--- src/modelo/Lectura.java
+++ src/modelo/Lectura.java
@@ -52,6 +52,40 @@ public class Lectura {
     }
     
     public void leerAeropuertos(String archAeropuertos,TreeMap<String,Ciudad> aeropuertos){
+        String linea;
+        String [] valor;
+        String continente="";
+        String codAeropuerto, ciudad, pais, abreviado;
+        int id;
+        
+        try{
+            BufferedReader bufer= new BufferedReader(new FileReader(archAeropuertos));
+            while((linea=bufer.readLine())!=null){
+                if(linea.contains("America del Sur")){
+                    continente="America del Sur";
+                    break;
+                }
+            }
+            while((linea=bufer.readLine())!=null){
+                if(linea.contains("Europa")){
+                    continente="Europa";
+                    continue;
+                }
+                valor=linea.trim().split("\t");
+                if(valor[0].isEmpty()) 
+                    continue;
+                id=Integer.parseInt(valor[0]);
+                codAeropuerto=valor[1];
+                ciudad=valor[2];
+                pais=valor[3];
+                abreviado=valor[4];
+                Ciudad aero= new Ciudad(id,codAeropuerto,ciudad,pais,abreviado,continente);
+                aeropuertos.put(codAeropuerto,aero);
+            }
+
+        }catch(IOException ex){
+            Logger.getLogger(Lectura.class.getName()).log(Level.SEVERE,null,ex);                        
+        }
     }
     
     public void leerVuelos(String archVuelos, ArrayList<Vuelo> vuelos){
